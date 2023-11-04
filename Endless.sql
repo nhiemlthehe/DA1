0c19d7d5f168518eb155c5e47ea1626696cace26
nhiemlthehe <nhiemltpc05396@fpt.edu.vn>
2023-11-04 20:39:27 +0700

    TaiCauLenhSQL

---
+++ Endless.sql
@@ -0,0 +1,139 @@
+﻿-- Tạo CSDL EndlessDB
+CREATE DATABASE EndlessDB;
+GO
+USE EndlessDB;
+GO
+
+-- Tạo bảng LoaiGiay
+CREATE TABLE LoaiGiay (
+    MaLoaiGiay INT PRIMARY KEY IDENTITY(1,1),
+    TenLoaiGiay NVARCHAR(50)
+);
+GO
+
+-- Tạo bảng MauSac
+CREATE TABLE MauSac (
+    MaMauSac INT PRIMARY KEY IDENTITY(1,1),
+    TenMauSac NVARCHAR(50)
+);
+GO
+
+-- Tạo bảng KichThuoc
+CREATE TABLE KichThuoc (
+    MaKichThuoc INT PRIMARY KEY IDENTITY(1,1),
+    TenKichThuoc NVARCHAR(10)
+);
+GO
+
+-- Tạo bảng KhuyenMai
+CREATE TABLE KhuyenMai (
+    MaKM INT PRIMARY KEY IDENTITY(1,1),
+    TenKM NVARCHAR(255),
+    PhanTram FLOAT,
+    NgayBatDau DATE,
+    NgayKetThuc DATE
+);
+GO
+
+-- Tạo bảng NhanVien
+CREATE TABLE NhanVien (
+    MaNV INT PRIMARY KEY IDENTITY(1,1),
+    TenNV NVARCHAR(255),
+    NgaySinh DATE,
+    DiaChi NVARCHAR(255),
+    SDT NVARCHAR(20),
+    Email VARCHAR(50),
+    MaTK VARCHAR(10) UNIQUE, 
+    MatKhau NVARCHAR(50)
+);
+GO
+
+-- Tạo bảng KhachHang
+CREATE TABLE KhachHang (
+    MaKH INT PRIMARY KEY IDENTITY(1,1),
+    TenKH NVARCHAR(255),
+    DiaChi NVARCHAR(255),
+    SDT NVARCHAR(20)
+);
+GO
+
+-- Tạo bảng SanPham
+CREATE TABLE SanPham (
+    MaSP INT PRIMARY KEY IDENTITY(1,1),
+    TenSP NVARCHAR(255),
+    Gia DECIMAL(18, 2),
+    SoLuong INT,
+    MaLoaiGiay INT,
+    MaKM INT,
+    KhuyenMai FLOAT,
+    FOREIGN KEY (MaLoaiGiay) REFERENCES LoaiGiay(MaLoaiGiay),
+    FOREIGN KEY (MaKM) REFERENCES KhuyenMai(MaKM)
+);
+GO
+
+-- Tạo bảng ChiTietSanPham
+CREATE TABLE ChiTietSanPham (
+    MaCTSP INT PRIMARY KEY IDENTITY(1,1),
+    MaSP INT,
+    MaMau INT,
+    MaKT INT,
+    SoLuong INT,
+    FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP),
+    FOREIGN KEY (MaMau) REFERENCES MauSac(MaMauSac),
+    FOREIGN KEY (MaKT) REFERENCES KichThuoc(MaKichThuoc)
+);
+GO
+
+-- Tạo bảng NhapHang
+CREATE TABLE NhapHang (
+    MaDNH INT PRIMARY KEY IDENTITY(1,1),
+    MaCTSP INT,
+    SoLuong INT,
+    NgayNhap DATE,
+    GhiChu NVARCHAR(100),
+    FOREIGN KEY (MaCTSP) REFERENCES ChiTietSanPham(MaCTSP)
+);
+GO
+
+-- Tạo bảng HoaDon
+CREATE TABLE HoaDon (
+    MaHD INT PRIMARY KEY IDENTITY(1,1),
+    MaKH INT,
+    MaNV INT,
+    NgayBan DATE,
+    TongTien FLOAT,
+    HTThanhToan NVARCHAR(20),
+    TienThanhToan FLOAT,
+    MaKM INT,
+    FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH),
+    FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV),
+    FOREIGN KEY (MaKM) REFERENCES KhuyenMai(MaKM)
+);
+GO
+
+-- Tạo bảng HoaDonChiTiet
+CREATE TABLE HoaDonChiTiet (
+    MaHDCT INT PRIMARY KEY IDENTITY(1,1),
+    MaHD INT,
+    MaCTSP INT,
+    SoLuong INT,
+    FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD),
+    FOREIGN KEY (MaCTSP) REFERENCES ChiTietSanPham(MaCTSP)
+);
+GO
+
+-- Tạo bảng DoiHang
+CREATE TABLE DoiHang (
+    MaDH INT PRIMARY KEY IDENTITY(1,1),
+    MaHDCT INT,
+    MaSP INT,
+    MaNV INT,
+    SoLuong INT,
+    LyDo NVARCHAR(255),
+    NgayDoi DATE,
+    SoTienThayDoi DECIMAL(18, 2),
+    FOREIGN KEY (MaHDCT) REFERENCES HoaDonChiTiet(MaHDCT),
+    FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP),
+    FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV)
+);
+GO
